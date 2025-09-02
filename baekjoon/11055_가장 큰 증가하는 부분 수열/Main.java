c0c870ac805633164fb7046abf8b292986ca3678
HSY <hs03130@outlook.kr>
2025-09-02 12:34:15 +0900

    title: 11055 가장 큰 증가하는 부분 수열

---
+++ baekjoon/11055_가장 큰 증가하는 부분 수열/Main.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringBuilder sb = new StringBuilder();
+	static StringTokenizer st;
+
+	static int N;
+	static int[] nums;
+	static int[] dp;
+	static int ans;
+
+	public static void main(String[] args) throws IOException {
+		N = Integer.parseInt(br.readLine());
+		nums = new int[N];
+		dp = new int[N];
+
+		st = new StringTokenizer(br.readLine());
+		for (int idx = 0; idx < N; idx++) {
+			nums[idx] = Integer.parseInt(st.nextToken());
+		}
+
+		dp[0] = nums[0];
+		ans = dp[0];
+		for (int n = 1; n < N; n++) {
+			dp[n] = nums[n];
+			for (int idx = 0; idx < n; idx++) {
+				if (nums[idx] < nums[n]) {
+					dp[n] = Math.max(dp[idx] + nums[n], dp[n]);
+				}
+			}
+			if (dp[n] > ans) {
+				ans = dp[n];
+			}
+		}
+
+		sb.append(ans);
+
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+	}
+
+}
